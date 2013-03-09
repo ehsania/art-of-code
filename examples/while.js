b33fc85a3218ddc13ac2e54ef57973b2c69f1a39
Ali Ehsanfar <ali.ehsanfar@gmail.com>
2013-03-09 16:48:40 +0330

    Update while.js

--- examples/while.js
+++ examples/while.js
@@ -2,7 +2,7 @@
 var initValue = 5;
 
 /**
- * This sample will easily increase the `initValue` and then compare it with `0`, Until `initValue` is equal to zero.
+ * This sample will easily decrease the `initValue` and then compare it with `0`, Until `initValue` is equal to zero.
  * The `while` condition actually is `initValue-- > 0`.
  *
  * More detail: http://stackoverflow.com/questions/1642028/what-is-the-name-of-this-operator
